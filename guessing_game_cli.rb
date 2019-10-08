07d81cdf080269835e5541460f62a09a4843e752
Kuran V Kumar <kurankumar@gmail.com>
2019-10-08 01:53:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random_numb = rand(6) + 1
   puts "Guess a number between 1 and 6"
   input = gets.chomp
-  if input == random_numb 
+  if input == random_numb.to_string
     puts "You guessed correclty!"
     
 end
